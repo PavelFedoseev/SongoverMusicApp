4f15cf5b5fbe5c340e00a2a0a91a38bd08e2b29e
mainMacBook <fedoseev.p.m@gmail.com>
2021-08-18 22:38:38 +0300

    Jetpack Compose: Добавлен элемент MusicSeekbar

--- app/src/main/java/com/project/songovermusicapp/exoplayer/source/LocalMediaSource.kt
+++ app/src/main/java/com/project/songovermusicapp/exoplayer/source/LocalMediaSource.kt
@@ -4,20 +4,23 @@ import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.project.songovermusicapp.exoplayer.source.State.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class LocalMediaSource constructor() {
+class LocalMediaSource constructor() : MediaSource {
 
     var songs = emptyList<MediaMetadataCompat>()
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
-    private var state: State = State.STATE_CREATED
+    private var state: State = STATE_CREATED
         set(value) {
-            if (value == State.STATE_INITIALIZED || value == State.STATE_ERROR) {
+            if (value == STATE_INITIALIZED || value == STATE_ERROR) {
                 synchronized(onReadyListeners) {
                     field = value
                     onReadyListeners.forEach { listener ->
-                        listener(state == State.STATE_INITIALIZED)
+                        listener(state == STATE_INITIALIZED)
                     }
                 }
             } else {
@@ -25,4 +28,20 @@ class LocalMediaSource constructor() {
             }
         }
 
+    override suspend fun fetchMedia() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+
+    }
+
+    override fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory): ConcatenatingMediaSource {
+        TODO("Not yet implemented")
+    }
+
+    override fun asMediaItems(): MutableList<MediaBrowserCompat.MediaItem> {
+        TODO("Not yet implemented")
+    }
+
+    override fun whenReady(action: (Boolean) -> Unit): Boolean {
+        TODO("Not yet implemented")
+    }
 }
